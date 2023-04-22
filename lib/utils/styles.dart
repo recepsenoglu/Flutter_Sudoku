2018bbed904c6282c1ceb99141f1937fd2c2fbb9
Recep Oğuzhan Şenoğlu <recep.senoglu.34@gmail.com>
2023-04-22 03:43:48 +0200

    App colors added

---
+++ lib/utils/styles.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class Styles {
+  static const _primaryColor = Color(0xFF325BAD);
+
+  static const _appBarBackgroundColor = Colors.white;
+  static const _appBarTitleColor = Color(0xFF2D4762);
+  static const _appBarActionsColor = Color(0xFF4561A7);
+
+  static const _backgroundColor = Colors.white;
+
+  static const _boardBorderColor = Colors.black;
+  static const _boardLineColor = Color(0xFFC2CAD5);
+
+  static const _cellColor = Colors.white;
+  static const _selectedCellColor = Color(0xFFB2DFFE);
+  static const _highlightedCellColor = Color(0xFFE3ECF3);
+  static const _highlightedNumberCellColor = Color(0xFFC4D6E8);
+  static const _wrongNumberCellColor = Color(0xFFFFCCD5);
+
+  static const _enteredNumberColor = _primaryColor;
+  static const _givenNumberColor = Colors.black;
+  static const _wrongNumberColor = Color(0xFFD70942);
+  static const _noteNumberColor = Color(0xFF99989C);
+  static const _highlightedNoteNumberColor = Color(0xFF99989C);
+
+  static const _actionButtonColor = Color(0xFF6F7583);
+
+  static const _numberButtonColor = Color(0xFF355DA9);
+  static const _noteButtonColor = Color(0xFF616B7D);
+
+  static Color get primaryColor => _primaryColor;
+
+  static Color get appBarBackgroundColor => _appBarBackgroundColor;
+  static Color get appBarTitleColor => _appBarTitleColor;
+  static Color get appBarActionsColor => _appBarActionsColor;
+
+  static Color get backgroundColor => _backgroundColor;
+
+  static Color get boardBorderColor => _boardBorderColor;
+  static Color get boardLineColor => _boardLineColor;
+
+  static Color get cellColor => _cellColor;
+  static Color get selectedCellColor => _selectedCellColor;
+  static Color get highlightedCellColor => _highlightedCellColor;
+  static Color get highlightedNumberCellColor => _highlightedNumberCellColor;
+  static Color get wrongNumberCellColor => _wrongNumberCellColor;
+
+  static Color get enteredNumberColor => _enteredNumberColor;
+  static Color get givenNumberColor => _givenNumberColor;
+  static Color get wrongNumberColor => _wrongNumberColor;
+  static Color get noteNumberColor => _noteNumberColor;
+  static Color get highlightedNoteNumberColor => _highlightedNoteNumberColor;
+
+  static Color get actionButtonColor => _actionButtonColor;
+
+  static Color get numberButtonColor => _numberButtonColor;
+  static Color get noteButtonColor => _noteButtonColor;
+}
