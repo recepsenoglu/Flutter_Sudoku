60c10dec2040feaebf04df0dbbb02faac5831650
Recep Oğuzhan Şenoğlu <recep.senoglu.34@gmail.com>
2023-04-22 02:57:59 +0200

    Game screen created

---
+++ lib/screens/game_screen/game_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GameScreen extends StatelessWidget {
+  const GameScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
